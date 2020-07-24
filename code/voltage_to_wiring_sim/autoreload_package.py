725e2e3f215dca3efad1d051640d0f1460965952
Tomas Fiers <tomas.fiers@gmail.com>
2020-07-24 17:27:49 +0200

    Only reload package if files modified

--- code/voltage_to_wiring_sim/autoreload_package.py
+++ code/voltage_to_wiring_sim/autoreload_package.py
@@ -1,5 +1,6 @@
 from importlib import reload
 from inspect import getmembers, getmodule
+from pathlib import Path
 from types import ModuleType
 
 
@@ -8,8 +9,19 @@ import voltage_to_wiring_sim
 
 def load_ipython_extension(ipython):
     ipython.events.register(
-        "pre_execute", lambda: reload_package(voltage_to_wiring_sim)
+        "pre_execute", lambda: reload_package_if_modified(voltage_to_wiring_sim)
     )
+
+
+def reload_package_if_modified(entrypoint: ModuleType):
+    global prev_last_modified_time
+    package_dir = Path(entrypoint.__spec__.submodule_search_locations[0])
+    module_files = package_dir.glob("**/*.py")
+    last_modified_time = max([f.stat().st_mtime for f in module_files])
+    print(last_modified_time, prev_last_modified_time)
+    if last_modified_time > prev_last_modified_time:
+        prev_last_modified_time = last_modified_time
+        reload_package(entrypoint)
 
 
 def reload_package(entrypoint: ModuleType):
@@ -21,13 +33,15 @@ def reload_package(entrypoint: ModuleType):
     visited_modules = set()
 
     def visit(module):
-        if module not in visited_modules:
+        if module in visited_modules:
+            return
+        else:
             visited_modules.add(module)
             if module.__package__.startswith(entrypoint.__package__):
                 for name, object in getmembers(module):
                     if (source_module := getmodule(object)) :
                         visit(source_module)
-                        
+
                 reload(module)
                 print(f"Reloaded {module.__name__}")
 
