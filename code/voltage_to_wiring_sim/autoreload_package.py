64aaf75571f16553790a60c7a08f494d9f838d4a
Tomas Fiers <tomas.fiers@gmail.com>
2020-07-24 17:48:59 +0200

    Make reload-on-modified-files-only work using class

--- code/voltage_to_wiring_sim/autoreload_package.py
+++ code/voltage_to_wiring_sim/autoreload_package.py
@@ -3,48 +3,49 @@ from inspect import getmembers, getmodule
 from pathlib import Path
 from types import ModuleType
 
-
 import voltage_to_wiring_sim
 
 
+class PackageAutoReloader:
+    def __init__(self, entrypoint: ModuleType):
+        self.entrypoint = entrypoint
+        self.mtime_at_last_interaction = self.get_mtime()
+
+    def get_mtime(self):
+        """ Last time when any of the Python files in the package were modified. """
+        package_dir = Path(self.entrypoint.__spec__.submodule_search_locations[0])
+        module_paths = package_dir.glob("**/*.py")
+        return max([path.stat().st_mtime for path in module_paths])
+
+    def reload_package_if_modified(self):
+        if (mtime := self.get_mtime()) > self.mtime_at_last_interaction:
+            self.mtime_at_last_interaction = mtime
+            self.reload_package()
+
+    def reload_package(self):
+        """
+        Follows the import tree from the `entrypoint` module -- but only to modules that
+        belong to the same package. Reloads all such modules in depth-first order.
+        """
+        visited_modules = set()
+
+        def visit(module):
+            if module in visited_modules:
+                return
+            else:
+                visited_modules.add(module)
+                if module.__package__.startswith(self.entrypoint.__package__):
+                    for name, object in getmembers(module):
+                        if (source_module := getmodule(object)) :
+                            visit(source_module)
+
+                    reload(module)
+                    print(f"Reloaded {module.__name__}")
+
+        visit(self.entrypoint)
+
+
 def load_ipython_extension(ipython):
-    ipython.events.register(
-        "pre_execute", lambda: reload_package_if_modified(voltage_to_wiring_sim)
-    )
+    autoreloader = PackageAutoReloader(voltage_to_wiring_sim)
+    ipython.events.register("pre_execute", autoreloader.reload_package_if_modified)
 
-
-def reload_package_if_modified(entrypoint: ModuleType):
-    global prev_last_modified_time
-    package_dir = Path(entrypoint.__spec__.submodule_search_locations[0])
-    module_files = package_dir.glob("**/*.py")
-    last_modified_time = max([f.stat().st_mtime for f in module_files])
-    print(last_modified_time, prev_last_modified_time)
-    if last_modified_time > prev_last_modified_time:
-        prev_last_modified_time = last_modified_time
-        reload_package(entrypoint)
-
-
-def reload_package(entrypoint: ModuleType):
-    """
-    Follows the import tree from the `entrypoint` module -- but only to modules that
-    belong to the same package. Reloads all such modules in depth-first order.
-    """
-
-    visited_modules = set()
-
-    def visit(module):
-        if module in visited_modules:
-            return
-        else:
-            visited_modules.add(module)
-            if module.__package__.startswith(entrypoint.__package__):
-                for name, object in getmembers(module):
-                    if (source_module := getmodule(object)) :
-                        visit(source_module)
-
-                reload(module)
-                print(f"Reloaded {module.__name__}")
-
-    # print(f"Reloading package `{entrypoint.__package__}`", end="...")
-    visit(entrypoint)
-    # print("Done")
