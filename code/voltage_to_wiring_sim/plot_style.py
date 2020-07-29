b81c387e4ece639d41b978742d56c0c830f43513
Tomas Fiers <tomas.fiers@gmail.com>
2020-07-29 09:25:17 +0200

    Fix figsize(): / and no // (and make method)

--- code/voltage_to_wiring_sim/plot_style.py
+++ code/voltage_to_wiring_sim/plot_style.py
@@ -1,52 +1,47 @@
 from copy import copy
-from dataclasses import dataclass
 
 import matplotlib as mpl
 
 
-@dataclass
-class FigSizeCalc:
-    text_vs_plot_size: float = 0.24
-    fig_width_pixels: int = 500
-    # `fig_width_pixels` is only approximate. Note also that 'retina display' in Jupyter
-    # Notebooks will double it.
-    fig_height_vs_width: float = 0.62
+def figsize(
+    aspect: float = 0.62, width: int = 500, rel_text_size: float = 1.33,
+):
+    """
+    :return:  A dictionary with keys "figsize" and "dpi" that can be passed as **kwargs
+              to matplotlib figure creation methods.
 
-    def __post_init__(self):
-        self.pixels_per_inch = 400 * self.text_vs_plot_size
-        self.fig_height_pixels = round(self.fig_width_pixels * self.fig_height_vs_width)
-        self.figsize = (
-            self.fig_width_pixels // self.pixels_per_inch,
-            self.fig_height_pixels // self.pixels_per_inch,
-        )
-        self.fig_kwargs = {
-            "figsize": self.figsize,
-            "dpi": self.pixels_per_inch,
-        }
+    :param aspect:  Fig height / fig width.
+    :param width:  Pixel width of figure.
+    :param rel_text_size:  Determines how large text is relative to other plot elements,
+                such as lines and ticks. (It is scaled so that it is "1" at the default
+                dpi of 72).
+    """
+    dpi = 72 * rel_text_size  # = pixels per inch
+    height = round(width * aspect)
+    return dict(figsize=(width / dpi, height / dpi), dpi=dpi)
+
+
+default_figsize = figsize()
 
 
 # fmt: off
 
 layout = {
-    "figure.autolayout": True,   # Auto-apply `tight_layout`. (This doesn't seem to do
+    "figure.autolayout": False,   # Auto-apply `tight_layout`. (This doesn't seem to do
                                  # anything yet for the current simple plots).
     "axes.spines.top": False,
     "axes.spines.right": False,
     "figure.facecolor": "white", # Avoid transparent background that make axis and tick
                                  # labels unreadable in a lot of image viewers (you
-                                 # often get this when a matplotlib figure is shared on
-                                 # Twitter).
+                                 # often see this when a matplotlib figure is eg shared
+                                 # on Twitter).
     "xtick.color": "grey",
     "ytick.color": "grey",
 }
 
-
-
-default_fig_size = FigSizeCalc()
-
 sizing = {
-    "figure.figsize": default_fig_size.figsize,
-    "figure.dpi": default_fig_size.pixels_per_inch,
+    "figure.figsize": default_figsize["figsize"],
+    "figure.dpi": default_figsize["dpi"],
     "xtick.labelsize": "small",     # Default is "medium"
     "ytick.labelsize": "small",
     "legend.fontsize": "small",     # Default is "medium"
@@ -56,8 +51,8 @@ sizing = {
 
 new_style = {**layout, **sizing}
 
-# This is not the same as mpl.rcParamsDefault in Jupyter Notebook
-# ("backend" is eg changed to "module://ipykernel.pylab.backend_inline")
+# This is not the same as mpl.rcParamsDefault, as in a Jupyter Notebook, "backend" is eg
+# changed to "module://ipykernel.pylab.backend_inline" in mpl.rcParams.
 original_rcParams = copy(mpl.rcParams)
 
 def reset_and_apply():
