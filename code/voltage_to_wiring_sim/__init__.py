169e0f24dfb378fe8c1894d790ee9876def96f3d
Tomas Fiers <tomas.fiers@gmail.com>
2020-08-05 12:08:43 +0200

    (__init__.py: sort imports)

--- code/voltage_to_wiring_sim/__init__.py
+++ code/voltage_to_wiring_sim/__init__.py
@@ -1,8 +1,8 @@
 # Import various modules and objects into the package namespace, so we have easy access
 # via tab-completion in an interactive session.
-from . import params, neuron_sim, synapses, plot_style, spike_train, units
-from .time_grid import TimeGrid
+from . import neuron_sim, params, plot_style, spike_train, synapses, units
 from .neuron_sim import simulate_izh_neuron
+from .plot_style import figsize
 from .spike_train import generate_Poisson_spike_train
 from .synapses import calc_synaptic_conductance
-from .plot_style import figsize
+from .time_grid import TimeGrid
