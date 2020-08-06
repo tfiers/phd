8d388cfb95360c962d17c63418fe65a213906803
Tomas Fiers <tomas.fiers@gmail.com>
2020-08-06 17:35:34 +0200

    Revert "on website build, add html file to server root for google site ownership verification"
    github pages won't serve this (admittedly malformed) html file :'(
    
    This reverts commit a2652733dda815e0a575be620ad310512b96e125.

--- website/build.py
+++ website/build.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
+
 import sys
-from shutil import copy
 from subprocess import run
 
 
@@ -10,6 +10,3 @@ def run_jb_cmd(cmd):
 
 run_jb_cmd("clean")
 run_jb_cmd("build")
-
-# Add html file to website root to prove to Google we own this website.
-copy("_static/googlea70385b87ca631e1.html", "_build/html/")
