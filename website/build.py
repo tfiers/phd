86599ac85e4236bcbe5e7a343404bb7e94bc0324
Tomas Fiers <tomas.fiers@gmail.com>
2020-08-06 18:44:27 +0200

    Add google meta tag not to refreshing index.html, but to /md/home.html

--- website/build.py
+++ website/build.py
@@ -3,6 +3,8 @@
 import sys
 from subprocess import run
 
+from lxml import etree
+
 
 def run_jb_cmd(cmd):
     run(["jupyter-book", cmd, "."], stdout=sys.stdout, stderr=sys.stderr)
@@ -10,20 +12,18 @@ def run_jb_cmd(cmd):
 
 def add_meta_tag():
     """ To prove to Google we own this website. """
-    # This file only contains a bare <meta> refresh tag.
-    index_html_file = "./_build/html/index.html"
-    with open(index_html_file) as f:
-        existing_lines = f.readlines()
-    new_lines = [
-        "<html><head>\n",
-        '<meta name="google-site-verification" content="QPn27BqA5LpMmT7Y7mFDLlWCeUw5aVcr74ShytJ3hJU" />\n',
-        *existing_lines,
-        "</head></html>\n",
-    ]
-    with open(index_html_file, "w") as f:
-        f.writelines(new_lines)
+    home_page_path = "./_build/html/md/home.html"
+    meta_tag_str = '<meta name="google-site-verification" content="QPn27BqA5LpMmT7Y7mFDLlWCeUw5aVcr74ShytJ3hJU" />'
+    parser = etree.HTMLParser()
+    tree = etree.parse(home_page_path, parser)
+    tree.find("head").append(etree.fromstring(meta_tag_str))
+    tree.write(home_page_path, method="html")
+    print(f"Added meta tag to {home_page_path}")
 
 
 run_jb_cmd("clean")
 run_jb_cmd("build")
-add_meta_tag()
+try:
+    add_meta_tag()
+except Exception as e:
+    print(e)
