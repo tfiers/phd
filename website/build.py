f40625f793ec67e43f32792e482e3fa391a84f5c
Tomas Fiers <tomas.fiers@gmail.com>
2020-08-06 19:52:34 +0200

    Add meta robots:noindex to every page on build

--- website/build.py
+++ website/build.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+from pathlib import Path
 from subprocess import run
 
 from lxml import etree
@@ -10,20 +11,31 @@ def run_jb_cmd(cmd):
     run(["jupyter-book", cmd, "."], stdout=sys.stdout, stderr=sys.stderr)
 
 
-def add_meta_tag():
-    """ To prove to Google we own this website. """
-    home_page_path = "./_build/html/md/home.html"
-    meta_tag_str = '<meta name="google-site-verification" content="QPn27BqA5LpMmT7Y7mFDLlWCeUw5aVcr74ShytJ3hJU" />'
+META_TAG_STRS = (
+    # Direct search engines to not index this page.
+    '<meta name="robots" content="noindex" />',
+    # Prove to Google we own this website.
+    '<meta name="google-site-verification" content="QPn27BqA5LpMmT7Y7mFDLlWCeUw5aVcr74ShytJ3hJU" />',
+)
+
+
+def add_meta_tags_to_all_pages():
+    html_file_paths = map(str, Path("./_build/html/").glob("**/*.html"))
     parser = etree.HTMLParser()
-    tree = etree.parse(home_page_path, parser)
-    tree.find("head").append(etree.fromstring(meta_tag_str))
-    tree.write(home_page_path, method="html")
-    print(f"Added meta tag to {home_page_path}")
+    # Must convert generator to tuple, as we'll use it more than once
+    meta_tags = tuple(map(etree.fromstring, META_TAG_STRS))
+    for html_file_path in html_file_paths:
+        try:
+            tree = etree.parse(html_file_path, parser)
+            head = tree.find("head")
+            for meta_tag in meta_tags:
+                head.append(meta_tag)
+            tree.write(html_file_path, method="html")
+            print(f"Added meta tags to {html_file_path}")
+        except:
+            print(f"Could not add meta tags to {html_file_path}")
 
 
 run_jb_cmd("clean")
 run_jb_cmd("build")
-try:
-    add_meta_tag()
-except Exception as e:
-    print(e)
+add_meta_tags_to_all_pages()
