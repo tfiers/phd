e63d5ce1bf28da9e8ad0e6a2c1998c1f940b6d44
Tomas Fiers <tomas.fiers@gmail.com>
2020-09-11 13:43:36 +0100

    📚DOC: submit to the black god in notebook_init.py

--- code/voltage_to_wiring_sim/notebook_init.py
+++ code/voltage_to_wiring_sim/notebook_init.py
@@ -7,29 +7,41 @@ This preloads packages, configures IPython, and populates the namespace with som
 objects.
 """
 
-# fmt: off
-
 
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
 print("Imported `np`, `mpl`, `plt")
 
 
+# -------------------------------------------------------------------------------------
+
+
 import voltage_to_wiring_sim as v
+
 print("Imported package `voltage_to_wiring_sim` as `v`")
 
 
+# -------------------------------------------------------------------------------------
+
+
 from voltage_to_wiring_sim.util import *
+
 # from voltage_to_wiring_sim.units import *
+
 print("Imported `*` from `v.util` and from `v.units`")
 
+
+# -------------------------------------------------------------------------------------
 
 # The following allows you to edit the source code while running a notebook, and then
 # use this updated code in the notebook, without having to restart the kernel on every
 # code change.
 # Note that this isn't foolproof, and kernel restarts may still be required.
+
 from IPython import get_ipython
+
 ipython = get_ipython()
 if ipython:  # Allow running as a script too (i.e. not in a notebook or REPL).
     ipython.run_line_magic("reload_ext", "autoreload")
@@ -39,30 +51,48 @@ if ipython:  # Allow running as a script too (i.e. not in a notebook or REPL).
     print("Setup autoreload")
 
 
+# -------------------------------------------------------------------------------------
+
 # If the last expression of a code cell is eg `product = 3 * 7`, and the cell is run,
 # IPython prints nothing, by default. Here, we make it print the result (`21`).
 # This avoids having to type an extra line with just `product` to see the result.
 # (Prints can still be suppressed by ending the line with `;`).
+
 from IPython.core.interactiveshell import InteractiveShell
+
 InteractiveShell.ast_node_interactivity = "last_expr_or_assign"
 
 
-# Double resolution plots (without taking up twice as much screen space).
-from IPython.display import set_matplotlib_formats
-set_matplotlib_formats('retina')
+# -------------------------------------------------------------------------------------
 
+# Double resolution plots (without taking up twice as much screen space).
+
+from IPython.display import set_matplotlib_formats
+
+set_matplotlib_formats("retina")
+
+
+# -------------------------------------------------------------------------------------
 
 # Print precision for (floating point) numbers.
+
+
 def set_np_precision(digits=4):
     # The ".4G" format allows eg `array([1000, 1E+05, 0.111])`
-    np.set_printoptions(formatter={
-        'float_kind': lambda x: format(x, f'.{digits}G'),
-    })
+    np.set_printoptions(
+        formatter={
+            "float_kind": lambda x: format(x, f".{digits}G"),
+        }
+    )
+
 
 set_np_precision()
 
 
+# -------------------------------------------------------------------------------------
+
 # Utility function from the Python standard library.
 # Given a function, returns a copy of that function, but with some arguments already
 # applied.
+
 from functools import partial
