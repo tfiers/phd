bdb4462083f97eff6face3c923d6056743e6cd20
Tomas Fiers <tomas.fiers@gmail.com>
2020-09-11 01:19:34 +0100

    👌IMPR: Back to `v.`…; simpler notebook_init …
    thanks to my `preload` package :)

---
+++ code/voltage_to_wiring_sim/__init__.py
@@ -0,0 +1,10 @@
+from preload import preload
+
+preload(["numpy", "matplotlib.pyplot"])
+
+
+from .neuron_sim import simulate_izh_neuron
+from .plot_style import figsize
+from .spike_train import generate_Poisson_spike_train
+from .synapses import calc_synaptic_conductance
+from .time_grid import TimeGrid
--- code/voltage_to_wiring_sim/notebook_init.py
+++ code/voltage_to_wiring_sim/notebook_init.py
@@ -9,58 +9,20 @@ objects.
 
 # fmt: off
 
-from IPython.display import display_markdown, clear_output
-from typing import List
 
-class MarkdownPrinter:
-    ''' Utility class to report what we do in this script. '''
-    
-    def __init__(self):
-        self.displayed_texts: List[str] = []
-    
-    def print(self, text: str, on_new_line=True):
-        if on_new_line:
-            display_markdown(text, raw=True)
-            self.displayed_texts.append(text)
-        else:  # On same line as last `print` call.
-            self.displayed_texts[-1] += text
-            clear_output()
-            for t in self.displayed_texts:
-                display_markdown(t, raw=True)
-
-md = MarkdownPrinter()
-
-
-md.print("Importing `np`, `mpl`, `plt`  … ")
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-md.print("✔", on_new_line=False)
+print("Imported `np`, `mpl`, `plt")
 
 
-md.print("Importing package `voltage_to_wiring_sim` as `v` … ")
 import voltage_to_wiring_sim as v
-md.print("✔", on_new_line=False)
+print("Imported package `voltage_to_wiring_sim` as `v`")
 
 
 from voltage_to_wiring_sim.util import *
-from voltage_to_wiring_sim.np_units import *
-md.print("Imported `*` from `v.util` and from `v.units` ✔")
-
-
-# Bookkeeping, see below.
-_existing_names = dir()
-
-# Import some commonly used methods and other objects directly into the global namespace
-from voltage_to_wiring_sim.neuron_sim import simulate_izh_neuron
-from voltage_to_wiring_sim.plot_style import figsize
-from voltage_to_wiring_sim.spike_train import generate_Poisson_spike_train
-from voltage_to_wiring_sim.synapses import calc_synaptic_conductance
-from voltage_to_wiring_sim.time_grid import TimeGrid
-
-# /bookkeeping, and reporting of what's newly imported
-new_names = set(dir()) - set(_existing_names) - {'_existing_names'}
-md.print(f"Imported {', '.join(f'`{name}`' for name in new_names)} ✔")
+# from voltage_to_wiring_sim.units import *
+print("Imported `*` from `v.util` and from `v.units`")
 
 
 # The following allows you to edit the source code while running a notebook, and then
@@ -74,7 +36,7 @@ if ipython:  # Allow running as a script too (i.e. not in a notebook or REPL).
     ipython.run_line_magic("autoreload", "2")
     # Exclude ourself from autoreloading, to avoid an exponential recursive import monster.
     ipython.run_line_magic("aimport", "-voltage_to_wiring_sim.notebook_init")
-    md.print("Setup autoreload ✔")
+    print("Setup autoreload")
 
 
 # If the last expression of a code cell is eg `product = 3 * 7`, and the cell is run,
