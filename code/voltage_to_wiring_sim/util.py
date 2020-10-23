5dc2a68d6dc8d12fb98dd3c003d978919a65ff85
Tomas Fiers <tomas.fiers@gmail.com>
2020-10-23 17:42:14 +0200

    ✨NEW add_scalebar()

--- code/voltage_to_wiring_sim/util.py
+++ code/voltage_to_wiring_sim/util.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 import numpy
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
+from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
+
+from voltage_to_wiring_sim.units import Quantity
 
 
 @contextmanager
@@ -31,3 +34,43 @@ def subplots(**kwargs) -> Tuple[Figure, Union[Axes, Sequence[Axes]]]:
 
 
 subplots.__doc__ = plt.subplots.__doc__
+
+
+def add_scalebar(
+    ax,
+    length: Quantity,
+    x: Union[float, Quantity] = 0.5,
+    y: Union[float, Quantity] = 0.5,
+    anchor="center",
+    label_top=True,
+    frame=False,
+    pad=0.3,
+    **kwargs,
+):
+
+    if isinstance(x, Quantity) and isinstance(y, Quantity):
+        loc_transform = ax.transData
+        x = x.display_data
+        y = y.display_data
+    elif isinstance(x, Quantity):
+        loc_transform = ax.get_xaxis_transform()
+        x = x.display_data
+    elif isinstance(y, Quantity):
+        loc_transform = ax.get_yaxis_transform()
+        y = y.display_data
+    else:
+        loc_transform = ax.transAxes
+
+    scalebar = AnchoredSizeBar(
+        transform=ax.transData,
+        size=length.item(),
+        label=str(length),
+        loc=anchor,
+        bbox_to_anchor=(x, y),
+        bbox_transform=loc_transform,
+        label_top=label_top,
+        frameon=frame,
+        pad=pad,
+        **kwargs,
+    )
+    ax.add_artist(scalebar)
