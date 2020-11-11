738d990d394e972e6d6bfe724bd2aa1a1740bca2
Tomas Fiers <tomas.fiers@gmail.com>
2020-11-11 18:41:14 +0100

    🔁REFACT move `add_scalebar()` to own module

--- codebase/voltage_to_wiring_sim/__init__.py
+++ codebase/voltage_to_wiring_sim/__init__.py
@@ -8,3 +8,4 @@ from .spike_train import generate_Poisson_spike_train
 from .synapses import calc_synaptic_conductance
 from .support.plot_style import figsize
 from .support.time_grid import TimeGrid
+from .support.scalebar import add_scalebar
--- codebase/voltage_to_wiring_sim/support/util.py
+++ codebase/voltage_to_wiring_sim/support/util.py
@@ -1,15 +1,10 @@
-from contextlib import contextmanager
 from dataclasses import asdict, dataclass
-from time import time
 from typing import Sequence, Tuple, Union
 
 import matplotlib.pyplot as plt
 import numpy
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
-from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
-
-from .units import Quantity
 
 
 @dataclass
@@ -42,43 +37,3 @@ def subplots(**kwargs) -> Tuple[Figure, Union[Axes, Sequence[Axes]]]:
 
 
 subplots.__doc__ = plt.subplots.__doc__
-
-
-def add_scalebar(
-    ax,
-    length: Quantity,
-    x: Union[float, Quantity] = 0.5,
-    y: Union[float, Quantity] = 0.5,
-    anchor="center",
-    label_top=True,
-    frame=False,
-    pad=0.3,
-    **kwargs,
-):
-
-    if isinstance(x, Quantity) and isinstance(y, Quantity):
-        loc_transform = ax.transData
-        x = x.display_data
-        y = y.display_data
-    elif isinstance(x, Quantity):
-        loc_transform = ax.get_xaxis_transform()
-        x = x.display_data
-    elif isinstance(y, Quantity):
-        loc_transform = ax.get_yaxis_transform()
-        y = y.display_data
-    else:
-        loc_transform = ax.transAxes
-
-    scalebar = AnchoredSizeBar(
-        transform=ax.transData,
-        size=length.value,
-        label=str(length),
-        loc=anchor,
-        bbox_to_anchor=(x, y),
-        bbox_transform=loc_transform,
-        label_top=label_top,
-        frameon=frame,
-        pad=pad,
-        **kwargs,
-    )
-    ax.add_artist(scalebar)
