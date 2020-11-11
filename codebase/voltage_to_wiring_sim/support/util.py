4b4d2fae70c4c71a1c866205464df26b6dc6d7b0
Tomas Fiers <tomas.fiers@gmail.com>
2020-11-11 18:38:05 +0100

    🔧MAINT remove `util.report_duration()`

--- codebase/voltage_to_wiring_sim/support/util.py
+++ codebase/voltage_to_wiring_sim/support/util.py
@@ -28,15 +28,6 @@ class QuantityCollection:
         return "\n".join(lines)
 
 
-@contextmanager
-def report_duration(action_description: str):
-    print(action_description, end=" … ")
-    t0 = time()
-    yield
-    duration = time() - t0
-    print(f"✔ ({duration:.2g} s)")
-
-
 def fix_rng_seed(seed=0):
     """
     Set seed of random number generator, to generate same random sequence in every
