85b90e8f70a70d1c38a583b536ebc665af845e14
Tomas Fiers <tomas.fiers@gmail.com>
2020-11-13 13:31:28 +0100

    📚DOC in main __init__.py, print feedback during import i.e. numba compiling

--- codebase/voltage_to_wiring_sim/__init__.py
+++ codebase/voltage_to_wiring_sim/__init__.py
@@ -2,6 +2,7 @@ from preload import preload
 
 preload(["numpy", "matplotlib.pyplot", "numba", "unitlib"])
 
+print("Importing from submodules (compiling numba functions)", end=" … ")
 
 from .neuron_sim import simulate_izh_neuron
 from .spike_train import generate_Poisson_spike_train
@@ -10,3 +11,6 @@ from .support.plot_style import figsize
 from .support.time_grid import TimeGrid
 from .support.scalebar import add_scalebar
 from .support.util import fix_rng_seed, pprint
+from .support.reproducibility import print_reproducibility_info
+
+print('✔\n')
