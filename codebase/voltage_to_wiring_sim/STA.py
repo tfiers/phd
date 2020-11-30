4e5106c80f3e99d5adc76e002e26e37b4a8e83b8
Tomas Fiers <tomas.fiers@gmail.com>
2020-11-30 15:18:28 +0100

    ⬆IMPR profile & speedup spike shuffling & STA calc

--- codebase/voltage_to_wiring_sim/STA.py
+++ codebase/voltage_to_wiring_sim/STA.py
@@ -1,9 +1,12 @@
 """
 Spike-triggered averaging.
 """
+from typing import List
 
 import matplotlib.pyplot as plt
 import numpy as np
+from numba import njit
+
 from .spike_train import spike_train_to_indices
 from .support.time_grid import TimeGrid
 from .support.units import Signal, mV, ms
@@ -14,15 +17,29 @@ def make_windows(
     spike_indices: np.ndarray,
     main_tg: TimeGrid,
     window_tg: TimeGrid,
-):
-    windows = []
-    for ix in spike_indices:
-        ix_end = ix + window_tg.N
-        if ix_end < main_tg.N:
-            windows.append(VI_signal[ix:ix_end])
+) -> np.ndarray:
+    return _make_windows(VI_signal, spike_indices, main_tg.N, window_tg.N)
 
-    windows = np.stack(windows)
-    return windows
+
+@njit(cache=True)
+def _make_windows(
+    VI_signal: np.ndarray,
+    spike_indices: np.ndarray,
+    main_N,
+    window_N,
+) -> np.ndarray:
+
+    num_windows = len(spike_indices)
+    windows = np.empty((num_windows, window_N))
+    for i in range(num_windows):
+        start_ix = spike_indices[i]
+        end_ix = start_ix + window_N
+        if end_ix < main_N:
+            windows[i, :] = VI_signal[start_ix:end_ix]
+        else:
+            num_windows -= 1
+
+    return windows[:num_windows, :]
 
 
 def calculate_STA(
@@ -32,9 +49,29 @@ def calculate_STA(
     window_tg: TimeGrid,
 ) -> Signal:
     spike_indices = spike_train_to_indices(spike_train)
-    windows = make_windows(VI_signal, spike_indices, main_tg, window_tg)
-    STA = windows.mean(axis=0)
+    # windows = make_windows(VI_signal, spike_indices, main_tg, window_tg)
+    # STA = np.mean(windows, axis=0)
+    STA = _calc_STA(VI_signal, spike_indices, main_tg.N, window_tg.N)
     return STA
+
+
+@njit(cache=True)
+def _calc_STA(
+    VI_signal: np.ndarray,
+    spike_indices: np.ndarray,
+    main_N,
+    window_N,
+) -> np.ndarray:
+    num_windows = len(spike_indices)
+    STA = np.zeros(window_N)
+    for i in range(num_windows):
+        start_ix = spike_indices[i]
+        end_ix = start_ix + window_N
+        if end_ix < main_N:
+            STA += VI_signal[start_ix:end_ix]
+        else:
+            num_windows -= 1
+    return STA / num_windows
 
 
 def plot_STA(STA: Signal, window_tg: TimeGrid, ax=None, **kwargs):
