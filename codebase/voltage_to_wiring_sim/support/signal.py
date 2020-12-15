25ec26c2a16921e53f6e50bb51f4d68e836e9e52
Tomas Fiers <tomas.fiers@gmail.com>
2020-12-15 11:55:47 +0100

    ✨NEW NDArrayWrapper base class (.ndim etc)

--- codebase/voltage_to_wiring_sim/support/signal.py
+++ codebase/voltage_to_wiring_sim/support/signal.py
@@ -6,7 +6,28 @@ from .units import Quantity
 
 
 @dataclass
-class Signal(np.lib.mixins.NDArrayOperatorsMixin):
+class NDArrayWrapper(np.lib.mixins.NDArrayOperatorsMixin):
+
+    data: np.ndarray
+
+    def __array__(self):
+        return self.data
+
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+
+
+@dataclass
+class Signal(NDArrayWrapper):
     """
     A NumPy array representing time series data. More precisely, a wrapper around a
     NumPy `ndarray`, with knowledge about the signal's timestep / sampling frequency,
@@ -18,30 +39,16 @@ class Signal(np.lib.mixins.NDArrayOperatorsMixin):
     data: np.ndarray
     timestep: Quantity
 
-    # See "Writing custom array containers"[1] from the NumPy manual for info on the
-    # below `__array__`, `__array_ufunc__`, and `__array_function__` methods.
-    #
-    # # [1](https://numpy.org/doc/stable/user/basics.dispatch.html)
-
-    # `Signal`'s base class `NDArrayOperatorsMixin` implements Python dunder methods
-    # like `__mul__` and `__imul__`, so that we can use standard Python syntax like `*`
-    # and `*=` with our `Signal`s.
-    #
-    # `NDArrayOperatorsMixin` implements these dunders by calling the corresponding
-    # NumPy ufunc (like `np.multiply`), which in turn defer to our `__array_ufunc__`
-    # method.
-
-    def __array__(self):
-        return self.data
-
     def __array_ufunc__(
         self, ufunc: np.ufunc, _method: str, *inputs, **kwargs
     ) -> "Signal":
-        np_result = ufunc(*map(np.asarray, inputs), **kwargs)
-        result = Signal(data=np_result, timestep=self.timestep)
+        input_arrays = map(np.asarray, inputs)
+        np_result = ufunc(*input_arrays, **kwargs)
+        result = Signal(np_result, self.timestep)
         return result
 
     def __array_function__(self, function, _types, args, kwargs):
-        np_result = function(*map(np.asarray, args), **kwargs)
-        result = Signal(data=np_result, timestep=self.timestep)
+        input_arrays = map(np.asarray, args)
+        np_result = function(*input_arrays, **kwargs)
+        result = Signal(np_result, self.timestep)
         return result
