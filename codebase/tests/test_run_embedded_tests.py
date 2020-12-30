be0b5934963b28af17de6ad53d61b48bef9caa87
Tomas Fiers <tomas.fiers@gmail.com>
2020-12-30 14:47:29 +0100

    📚DOC weird test file/func name

--- codebase/tests/test_run_embedded_tests.py
+++ codebase/tests/test_run_embedded_tests.py
@@ -1,3 +1,8 @@
+"""
+Weird file name and function name ("test..tests") is because pytest's default automatic
+test discovery expects files and functions to start with "test_"
+"""
+
 import voltage_to_wiring_sim as v
 
 
