98cd8d0424f085385aa811a1439ca1e3e3dc8c17
Tomas Fiers <tomas.fiers@gmail.com>
2021-01-01 14:00:38 +0100

    ⬆IMPR TimeGrid (bounds, i_slice, start)
    for use as plot zoom window

--- codebase/voltage_to_wiring_sim/support/time_grid.py
+++ codebase/voltage_to_wiring_sim/support/time_grid.py
@@ -2,19 +2,19 @@ from dataclasses import dataclass
 
 import numpy as np
 
-from .units import Array, Quantity
+from .units import Quantity, second
 
 
 @dataclass
 class TimeGrid:
-    duration: Quantity  # simulation duration
-    timestep: Quantity  # timestep
-    N: int = ...  # number of simulation steps
+    duration: Quantity
+    timestep: Quantity
+    start: Quantity = 0 * second
 
     def __post_init__(self):
-        self.N = round(self.duration / self.timestep)
-
-    @property
-    def time(self) -> Array:
-        """ time array, for plotting """
-        return np.linspace(0, self.duration, self.N, endpoint=False)
+        self.N = round(self.duration / self.timestep)  # number of time bins
+        self.bounds = self.start + np.array([0, self.duration])
+        self.time = np.linspace(*self.bounds, self.N, endpoint=False)  # for plotting
+        index_bounds = np.round(self.bounds / self.timestep).astype(int)
+        self.i_slice = slice(*index_bounds)  # to index into a Signal with the same
+        #                                      timestep.
