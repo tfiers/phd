6bb515fe03f4b9c40d6678ba8f1461e80a33b794
Tomas Fiers <tomas.fiers@gmail.com>
2021-01-07 14:46:27 +0100

    ⬆IMPR plot style: ax grid :) 🗺

--- codebase/voltage_to_wiring_sim/support/plot_style.py
+++ codebase/voltage_to_wiring_sim/support/plot_style.py
@@ -9,14 +9,14 @@ def figsize(
     rel_text_size: float = 1.33,
 ):
     """
-    :return: A dictionary with keys "figsize" and "dpi" that can be passed as **kwargs
-             to matplotlib figure creation methods.
-
     :param aspect:  Fig width / fig height.
     :param width:  Pixel width of figure.
     :param rel_text_size:  Determines how large text is relative to other plot elements,
                 such as lines and ticks. (It is scaled so that it is "1" at the default
                 dpi of 72).
+
+    :return: A dictionary with keys "figsize" and "dpi" that can be passed as **kwargs
+             to matplotlib figure creation methods.
     """
     dpi = 72 * rel_text_size  # = pixels per inch
     height = round(width / aspect)
@@ -28,7 +28,7 @@ default_figsize = figsize()
 
 # fmt: off
 
-layout = {
+paint = {
     "figure.autolayout": False,   # Auto-apply `tight_layout`. (This doesn't seem to do
                                  # anything yet for the current simple plots).
     "axes.spines.top": False,
@@ -37,8 +37,14 @@ layout = {
                                  # labels unreadable in a lot of image viewers (you
                                  # often see this when a matplotlib figure is eg shared
                                  # on Twitter).
-    "xtick.color": "grey",
-    "ytick.color": "grey",
+    "axes.grid": True,
+    "grid.color": "0.85",  # Very light gray
+    "ytick.left": False,  # No ticks, but still labels
+    "xtick.bottom": False,
+    "xtick.color": "0.4",
+    "ytick.color": "0.4",
+    "xtick.major.pad": 0.5,
+    "ytick.major.pad": 0.5,
 }
 
 sizing = {
@@ -47,15 +53,13 @@ sizing = {
     "xtick.labelsize": "small",     # Default is "medium"
     "ytick.labelsize": "small",
     "legend.fontsize": "small",     # Default is "medium"
-    "xtick.major.size": 5,          # Default is 3.5
-    "ytick.major.size": 5,
 }
 
-new_style = {**layout, **sizing}
+new_style = {**paint, **sizing}
 
-# This is not the same as mpl.rcParamsDefault, as in a Jupyter Notebook, "backend" is eg
-# changed to "module://ipykernel.pylab.backend_inline" in mpl.rcParams.
 original_rcParams = copy(mpl.rcParams)
+#   This is not the same as mpl.rcParamsDefault, as in a Jupyter Notebook, "backend" is
+#   eg changed to "module://ipykernel.pylab.backend_inline" in mpl.rcParams.
 
 def reset_and_apply():
     # Reset base style before applying new style, so we can interactively experiment
