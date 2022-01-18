1057dad4c606e79b21a2d3a4bfa9abff8c1a462d
Tomas Fiers <tomas.fiers@gmail.com>
2022-01-18 03:06:42 +0000

    rename: plot/ » myplotlib/

--- src/VoltageToMap.jl
+++ src/VoltageToMap.jl
@@ -2,12 +2,12 @@ module VoltageToMap
 
 using FilePaths: @p_str
 
-include(p"plot/imports.jl")
-include(p"plot/plot.jl"); export plot
-include(p"plot/set.jl"); export set, legend, hylabel
-include(p"plot/colors.jl"); export mix, lighten, darken, toRGBAtuple, deemph, lightgrey,
-                                   C0, C1, C2, C3, C4, C5, C6, C7, C9, C10, mplcolors
-include(p"plot/style.jl"); export mplstyle
+include(p"myplotlib/imports.jl")
+include(p"myplotlib/plot.jl"); export plot
+include(p"myplotlib/set.jl"); export set, legend, hylabel
+include(p"myplotlib/colors.jl"); export mix, lighten, darken, toRGBAtuple, deemph, lightgrey,
+                                        C0, C1, C2, C3, C4, C5, C6, C7, C9, C10, mplcolors
+include(p"myplotlib/style.jl"); export mplstyle
 
 include("signal.jl"); export Signal, duration
 include("show.jl")
