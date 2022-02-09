3b181b0beb0d17cfd30ba8fa7e59c2b26c6c01e8
Tomas Fiers <tomas.fiers@gmail.com>
2022-02-09 11:40:40 +0000

    capture stderr, ]add Suppressor

--- notebooks/nb_init.jl
+++ notebooks/nb_init.jl
@@ -1,4 +1,4 @@
-using WhatIsHappening, Pkg
+using WhatIsHappening, Suppressor, Pkg
 
 # Filter out annoying deprecation warning on PyPlot import. Can be removed and replaced by
 # `prettify_logging_in_notebook!()` once this is released: https://github.com/JuliaPy/PyCall.jl/pull/950
@@ -6,14 +6,15 @@ using LoggingExtras
 hasdepwarning(log) = startswith(log.message, "`vendor()` is deprecated")
 global_logger(ActiveFilteredLogger(!hasdepwarning, get_pretty_notebook_logger()))
 
-@withfeedback Pkg.resolve()
-
-@withfeedback using Revise
-@withfeedback import Distributions
-@withfeedback import PyPlot
-@withfeedback import DataFrames, PrettyTables
-@withfeedback import MyToolbox
-@withfeedback using VoltageToMap
+hidden_stderr_info = @capture_err begin
+    @withfeedback Pkg.resolve()
+    @withfeedback using Revise
+    @withfeedback import Distributions
+    @withfeedback import PyPlot
+    @withfeedback import DataFrames, PrettyTables
+    @withfeedback import MyToolbox
+    @withfeedback using VoltageToMap
+end
 
 # Cannot be reexported from a package (i.e. MyToolbox / VoltageToMap), as it then clashes
 # with Base's `/`. `include`ing it does work.
