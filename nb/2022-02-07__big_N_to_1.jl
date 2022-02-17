11795755fe14a418040856a0d4fd1df5aad82654
Tomas Fiers <tomas.fiers@gmail.com>
2022-02-17 07:21:31 +0000

    spike gen in VectorContCb; CompVec IDs, postsynapses

--- nb/2022-02-07__big_N_to_1.jl
+++ nb/2022-02-07__big_N_to_1.jl
@@ -23,7 +23,7 @@
 include("nb_init.jl")
 
 using Parameters, ComponentArrays
-@alias CArray = ComponentArray;
+@alias CVec = ComponentVector;
 
 save(fname) = savefig(fname, subdir="methods");
 
@@ -34,13 +34,15 @@ sim_duration = 0.11 * seconds
 
 # ### Input spikers
 
-N_unconn = 100
-N_exc    = 5200
-N_inh    = N_exc ÷ 4
+# +
+# N_unconn = 100
+# N_exc    = 5200
+# N_inh    = N_exc ÷ 4
 
 N_unconn = 1
 N_exc    = 8
 N_inh    = N_exc ÷ 4
+# -
 
 N_conn = N_inh + N_exc
 
@@ -48,43 +50,36 @@ N = N_conn + N_unconn
 
 input_spike_rate = LogNormal_with_mean(4Hz, √0.6)  # See the previous notebook
 
-# + [markdown] heading_collapsed=true
 # ### Synapses
 
-# + [markdown] hidden=true
 # Reversal potential at excitatory and inhibitory synapses,  
 # as in the report [`2021-11-11__synaptic_conductance_ratio.pdf`](https://github.com/tfiers/phd-thesis/blob/main/reports/2021-11-11__synaptic_conductance_ratio.pdf):
 
-# + hidden=true
 v_exc =   0 * mV
 v_inh = -65 * mV;
 
-# + [markdown] hidden=true
-# Exponential decay time constant of synaptic conductance `g`, $τ_{s}$ (`s` for "synaptic"):
+# Synaptic conductances `g` at `t = 0`:
 
-# + hidden=true
-τ_s =   7 * ms;
-
-# + [markdown] hidden=true
-# Synaptic conductances at `t = 0`:
-
-# + hidden=true
 g0 = 0 * nS;
 
-# + [markdown] heading_collapsed=true
+# Exponential decay time constant of synaptic conductance, $τ_{s}$ (`s` for "synaptic"):
+
+τ_s = 7 * ms;
+
+# Increase in synaptic conductance on a presynaptic spike.
+
+Δg = 3 * nS;
+
 # ### Izhikevich neuron
 
-# + [markdown] hidden=true
 # Membrane potential `v` and adaptation variable `u` at `t = 0`:
 
-# + hidden=true
 v0    = -80 * mV
 u0    =   0 * pA;
 
-# + [markdown] hidden=true
 # Parameters for a cortical regular spiking neuron:
 
-# + hidden=true
+# +
 @with_kw struct IzhikevichParams
     C      = 100 * pF
     k      = 0.7 * (nS/mV)
@@ -97,114 +92,177 @@ u0    =   0 * pA;
     d      = 100 * pA       # `u` increase on spike. Free parameter.
 end
 
-cortical_RS = IzhikevichParams();
+cortical_RS = IzhikevichParams()
+
+# Fix these params globally for now.
+@unpack C, k, b, v_r, v_t, v_peak, c, a, d = cortical_RS;
 # -
 
-# ## IDs
+# ## Neurons & synapses
 
 # Simple here for the N-to-1 case: only input 'neurons' get an ID, and there is only one synapse for every (connected) neuron.
 
+@alias NeuronID = Int
+@alias SynapseID = Int;
+
+"""Given group names and numbers, build a CVec with these group names and a unique ID for each element."""
+function ID_CVec(; kw...)
+    cv = CVec(; [key => fill(-1, num) for (key,num) in kw]...)
+    cv[:] = 1:length(cv)
+    return cv
+end;
+
 # ### Neuron IDs
 
-neuron_ids = CArray(exc = 1:N_exc, inh = 1:N_inh, unconn = 1:N_unconn)
+ID_CVec(exc = N_exc, inh = N_inh, unconn = N_unconn)
 
-only(getaxes(neuron_ids))
+neuron_ids[end], labels(neuron_ids)[end]
 
-resetrng!(797)
-showsome(labels(neuron_ids))
-
-# i.e. a neuron's **global** ID = its index into the [ComponentVector](https://github.com/jonniedie/ComponentArrays.jl) "`neuron_ids`".
-
-# + [markdown] heading_collapsed=true
 # ### Synapse IDs
 
-# + hidden=true
-synapse_ids = CArray(exc = 1:N_exc, inh = 1:N_inh)
+synapse_ids = ID_CVec(exc = N_exc, inh = N_inh)
+
+# ### Connections
+
+# +
+using DataStructures: OrderedDict
+
+postsynapses = OrderedDict{NeuronID, Vector{SynapseID}}()
+
+for (n,s) in zip(neuron_ids.exc, synapse_ids.exc)
+    postsynapses[n] = [s]
+end
+for (n,s) in zip(neuron_ids.inh, synapse_ids.inh)
+    postsynapses[n] = [s]
+end
+
+postsynapses
 # -
 
-# ## Inputs
+# ## Sim
+
+# ### ISI distributions
 
 # Generate firing rates $λ$ by sampling from the input spike rate distribution.
 
 λ = rand(input_spike_rate, N)
 showsome(λ)
 
-# Alternative Exp parametrisation: scale $β$ = 1 / rate.
+# `Distributions.jl` uses alternative Exp parametrisation with scale $β$ = 1 / rate.
 
 β = 1 ./ λ
 ISI_distributions = Exponential.(β);
 #   This uses julia's broadcasting `.` syntax: make an `Exponential` distribution for every value in the β vector
 
-# Create $E_i$: for each neuron, the reversal potential at its downstream synapses.
+# ### Init spike times
 
-E = CArray(exc=fill(v_exc, N_exc), inh=fill(v_inh, N_inh))
+# Generate the first spike time for every input neuron by sampling once from its ISI distribution.
 
-# ## Sim
+first_spiketime_per_neuron = rand.(ISI_distributions);
 
-neuron_ids
+# Sort these initial spike times by building a priority queue.
 
 using DataStructures: PriorityQueue
 
-# +
-first_spiketimes = rand.(ISI_distributions)
-
-pq = PriorityQueue{Int, Float64}()
-for (neuron_ID, t) in enumerate(first_spiketimes)
-    enqueue!(pq, neuron_ID => t)
+next_input_spikes = PriorityQueue{NeuronID, Float64}()
+for (neuron_ID, t) in enumerate(first_spiketime_per_neuron)
+    enqueue!(next_input_spikes, neuron_ID => t)
 end
 
-next_spike_time, neuron_ID = dequeue_pair!(pq)
+# Pop off the top of the heap to find the first spiker.
 
-# t = 0s
-# while t < sim_duration
-#     input_neuron, t = dequeue_pair!(pq)  # earliest spike
-#     new_ISI = rand(ISI_distributions[input_neuron])
-#     enqueue!(pq, input_neuron => t + new_ISI)
-# end
-# -
+first_input_spike_time, first_input_spike_neuron = dequeue_pair!(next_input_spikes)
+
+# ### Differential equations 
 
 using OrdinaryDiffEq
 
-# +
-function f(D, vars, params, _t)
+# The derivative function that defines the continuous differential equations:
+
+function f(D, vars, _, _)
     @unpack v, u, g = vars
-    @unpack C, k, b, v_r, v_t, v_peak, c, a, d = params
-    I_s = sum(g .* (v .- E))
+    I_s = sum(g .* (v .- E))  # Sum synaptic currents.
         # Membrane current is by convention positive if positive charges are flowing out of the cell.
-        # For v = -80 mV and v_s = 0 mV, we get negative I_s, i.e. charges flowing in ✔.
+        # For e.g. v = -80 mV and E = 0 mV, we get negative I_s, i.e. charges flowing in ✔.
     D.v = (k * (v - v_r) * (v - v_t) - u - I_s) / C
     D.u = a * (b * (v - v_r) - u)
     D.g = -g ./ τs
     return nothing
+end;
+
+# `E` is simply a vector with for each neuron the reversal potential at its downstream synapses.
+
+E = CVec(exc = fill(v_exc, N_exc), inh = fill(v_inh, N_inh))
+
+# ### Events
+
+# +
+events = (
+    thr_crossing          = 1,
+    input_spike_generated = 2,
+)
+
+function update_distance_to_next_event(distance, vars, t, integrator)
+    v = vars.v
+    p = integrator.p  # params
+    distance[events.thr_crossing]          = v - v_peak
+    distance[events.input_spike_generated] = t - p.next_input_spike_time
 end
 
-# distance_to_thr_crossing(vars, _t, integrator) = integrator.p.v_peak - vars.v
-distance_to_thr_crossing(vars, _t, integrator) = v0 - vars.v
-    # Function that is zero at desired event.
-
-function on_thr_crossing(integrator)
-    vars, params = integrator.u, integrator.p
-    vars.v = v0
-#     vars.v = params.c
-#     vars.u += params.d
+function on_event(integrator, event)
+    vars = integrator.u
+    @unpack p, t = integrator  # params, time
+    
+    if event == events.thr_crossing
+        # The discontinuous LIF/Izhikevich/AdEx update
+        vars.v = c
+        vars.u += d
+        
+    elseif event == events.spike_generated
+        # Generate a new spike for the just fired input neuron.
+        fired_neuron = p.next_input_spike_neuron
+        new_ISI = rand(ISI_distributions[fired_neuron])
+        enqueue!(next_input_spikes, fired_neuron => t + new_ISI)
+        # Update the downstream synapses (one or zero in this case).
+        # Also note: no tx delay.
+        vars.g[postsynapses[fired_neuron]] .+= Δg
+        # Update params: find next spike.
+        p.next_input_spike_neuron, p.next_input_spike_time = dequeue_pair!(next_input_spikes)
+    end
 end
+# -
 
-cb = ContinuousCallback(distance_to_thr_crossing, on_thr_crossing)
+# ### Run simulation
 
-x0 = ComponentArray{Float64}(v = v0, u = u0, g = fill(g0, N_conn))  # Note eltype cast to float.
-prob = ODEProblem(f, x0, float(sim_duration), cortical_RS)  # Time must also be float.
+# Bring it all together (initial conditions, derivatives function, events) and solve.
+
+vars_t0 = CVec{Float64}(  # Note the cast to float, so that vars are float during sim.
+    v = v0, 
+    u = u0, 
+    g = fill(g0, N_conn),
+)
+
+params = CVec(
+    next_input_spike_time   = first_input_spike_time,
+    next_input_spike_neuron = first_input_spike_neuron,
+)
+
+# +
+prob = ODEProblem(f, vars_t0, float(sim_duration), params)
+    # Duration must be float too, so that `t` variable is float.
+
 @time sol = solve(
     prob,
-    Tsit5();          # The default solver. A Runge-Kutta method. Tsitouras 2011.
+    Tsit5();          # The default and recommended solver. A Runge-Kutta method. Tsitouras 2011.
     dt = Δt,          # Size of first step.
     adaptive = true,  # Take larger steps when output is steady.
     reltol = 1e-8,    # default: 1e-2
     abstol = 1e-8,    # default: 1e-6
-    callback = cb,
+    callback = VectorContinuousCallback(update_distance_to_next_event, on_event, length(events)),
 );
 # -
 
-# Tolerances from https://diffeq.sciml.ai/stable/tutorials/ode_example/#Choosing-a-Solver-Algorithm and experimentation:  
+# Tolerances are from https://diffeq.sciml.ai/stable/tutorials/ode_example/#Choosing-a-Solver-Algorithm and experimentation:  
 # Lower for either gives incorrect oscillations in steady state (non-todo: show this in a separate nb).
 
 plot(sol.t/ms, sol[1,:]/mV);
