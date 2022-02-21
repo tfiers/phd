ce9811ead726dcb16ff5395b2c3ecd9c6fb91c68
Tomas Fiers <tomas.fiers@gmail.com>
2022-02-21 22:24:44 +0000

    units: whitespace & comment cleanup

--- pkg/VoltageToMap/src/units.jl
+++ pkg/VoltageToMap/src/units.jl
@@ -2,14 +2,14 @@
 # 'Fake' units, until a better solution exists.
 # (`Unitful` works great, but the interactive UX is bad because of its very verbose types).
 
-giga = 1e9
-mega = 1e6
-kilo = 1e3
+giga  = 1e9
+mega  = 1e6
+kilo  = 1e3
 milli = 1e-3
 centi = 1e-2
 micro = 1e-6
-nano = 1e-9
-pico = 1e-12
+nano  = 1e-9
+pico  = 1e-12
 export mega, kilo, milli, centi, micro, nano, pico
 
 seconds = s = 1
@@ -61,10 +61,10 @@ gibi = 2^30
 # Sidenote: Windows uses binary prefixes, but displays them as SI: "MB" is actually "MiB".
 
 bytes = 1
-# We choose bytes as base unit (instead of bits), as that is what e.g. `sizeof` and
-# `Base.summarysize` report.
+    # We choose bytes as base unit (instead of bits), as that is what e.g. `sizeof` and
+    # `Base.summarysize` report.
 bits = bytes // 8
-kB = kilo * bytes  # note that Windows displays k
+kB = kilo * bytes
 MB = mega * bytes
 GB = giga * bytes
 kiB = kibi * bytes
