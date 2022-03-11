0537382e4decef906fd13e9e30cd53e84de72507
Tomas Fiers <tomas.fiers@gmail.com>
2022-03-11 07:20:45 +0000

    setup.jl: VtM≺Spl, & Pkg.rm fix for all local pkgs
    "LoadError: expected package {devdep} to be registered" is not only for main proj.
    It's also for VoltageToMap now (w/ Sciplotlib dep).

--- setup.jl
+++ setup.jl
@@ -1,14 +1,15 @@
-# Generate a new `Manifest.toml`, both for the main project, and the packages in `pkg/`.
-# This script is not needed if you install from the `Manifest.toml` commited to the
-# repository and don't hack on the packages (i.e. if you just want to reproduce results).
+# Generate a new `Manifest.toml`, both for the main project and the packages in `pkg/`.
+#
+# This script is not needed if you `instantiate` from the `Manifest.toml` commited to the
+# repository, and don't hack on the packages (i.e. if you just want to reproduce results).
 #
 # More specifically, run this script to instantiate the Julia projects in this repository
-# (i.e. for each, create a `Manifest.toml` with all its dependencies, and install those),
-# with the specification that, for inter-dependencies _within_ this project, the
-# local/development versions of the packages present in this repo must be used
-# (`Pkg.develop`).
+# That is, for each, create a `Manifest.toml` with all its dependencies, and install those,
+# with the specification that, for inter-dependencies _within_ this repository, the
+# local/development versions of the packages in this repo must be used (`Pkg.develop`).
 
-using Pkg, TOML
+using Pkg
+using TOML
 
 const reporoot = @__DIR__
 const pkgdir = joinpath(reporoot, "pkg")
@@ -24,36 +25,36 @@ const local_project_dependencies = [
     WhatIsHappening  => [],
     Sciplotlib       => [],
     MyToolbox        => [],
-    VoltageToMap     => [MyToolbox],
+    VoltageToMap     => [MyToolbox, Sciplotlib],
     mainproj         => [WhatIsHappening, MyToolbox, Sciplotlib, VoltageToMap],
-                        # gotta add MyTB as well, for the fix below to work.
+                        # We must add indirect local deps as well, for the fix below to work.
 ]
-# Note that these are sorted, with the higher level projects last.
+# Note that the list entries are sorted, with the higher level projects last.
 
 function install_local_projects()
     for (projdir, depdirs) in local_project_dependencies
         cd(projdir)
-        rm("Manifest.toml", force=true)
+        rm("Manifest.toml", force = true)  # (`force` is just to not error if not exists).
         Pkg.activate(".")
-        projdir == mainproj && apply_expected_registered_fix(depdirs)
+        @info "Temporarily removing dev dependencies from current `Project.toml`"
+        # Without this fix, we'd get "LoadError: expected package {devdep} to be registered"
+        # when calling `Pkg.develop` below.
         for depdir in depdirs
-            Pkg.develop(path=relpath(depdir))
+            try
+                Pkg.rm(pkgname(depdir))
+            catch
+                # The dependency is already removed from `Project.toml`.
+            end
         end
-        Pkg.instantiate()
+        for depdir in depdirs
+            Pkg.develop(path = relpath(depdir))
+        end
+        Pkg.instantiate()  # Install all dependencies.
     end
 end
 
-function apply_expected_registered_fix(depdirs)
-    @info "Temporarily removing dev dependencies from main `Project.toml`"
-    # Without this fix, we get "LoadError: expected package {devdep} to be registered".
-    # This fix is only necessary for the (main) project, not the packages. Pkg.jl bug?
-    for depdir in depdirs
-        pkgname = joinpath(depdir, "Project.toml") |> TOML.parsefile |> d -> d["name"]
-        try Pkg.rm(pkgname)
-        catch # The dependency is already removed from `Project.toml`.
-        end
-    end
-end
+pkgname(pkgdir) = joinpath(pkgdir, "Project.toml") |> TOML.parsefile |> dict -> dict["name"]
+
 
 install_local_projects()
 println("\n💃 All done")
