1781cac85b39f5a58f757c98877c561bdb4d465f
Tomas Fiers <tomas.fiers@gmail.com>
2022-04-29 08:47:15 +0100

    rename `vimsig`→`vi`

--- pkg/VoltageToMap/src/sim.jl
+++ pkg/VoltageToMap/src/sim.jl
@@ -5,12 +5,12 @@ include("sim/step.jl")
 function sim(params::SimParams)
     state, rec = init_sim(params)
     @unpack duration, num_timesteps = params
-    @showprogress x for i in 1:num_timesteps
+    @showprogress x "Running simulation: " for i in 1:num_timesteps
         step_sim!(state, params, rec, i)
     end
     t = linspace(zero(duration), duration, num_timesteps)
-    vimsig = add_VI_noise(rec.v, params.imaging.σ_noise)
-    return (; t, rec.v, vimsig, rec.input_spikes, state)
+    vi = add_VI_noise(rec.v, params.imaging.σ_noise)
+    return (; t, rec.v, vi, rec.input_spikes, state)
 end
 
 const x = progress_bar_update_interval = 400ms
