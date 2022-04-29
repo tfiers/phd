b05f42b949bdedb5668391a998345a92c70b49db
Tomas Fiers <tomas.fiers@gmail.com>
2022-04-29 08:48:05 +0100

    🔬 plot perf over N range

--- nb/2022-04-28__interpolate_N_from_30_to_6000.jl
+++ nb/2022-04-28__interpolate_N_from_30_to_6000.jl
@@ -43,147 +43,77 @@ using MyToolbox
 
 using VoltageToMap
 
-# ## Params & sim
+# ## Params
 
+N_excs = [
+    4,   # => N_inh = 1
+    17,  # Same as in `previous_N_30_input`.
+    80,
+    320,
+    1280,
+    5200,
+];
 
-# Short warm-up run. Get compilation out of the way.
-
-warmup_params = SimParams(
-        input = previous_N_30_input,
-        duration = 1 * minutes
-);
-
-@time sim(warmup_params);
-
-p = ExperimentParams(
+get_params(N_exc) = ExperimentParams(
     sim = SimParams(
-        input = realistic_N_6600_input,
-        duration = 0.2 * minutes,
-    )
+        duration = 10 * minutes,
+        imaging = get_VI_params_for(cortical_RS, spike_SNR = Inf),
+        input = PoissonInputParams(; N_exc),
+    ),
 );
-dumps(p)
 
-t, v, vimsig, input_spikes = @time sim(p.sim);
+param_sets = get_params.(N_excs);
 
-num_spikes = length.(input_spikes)
+dumps(param_sets[1])
 
-# ## Plot
+# ## Run
+
+perfs = Vector()
+for params in param_sets
+    @show params.sim.input.N_conn
+    perf = performance_for(params)
+    @show perf
+    push!(perfs, perf)
+    println()
+end
+
+# ## Plot results
 
 import PyPlot
 
 using VoltageToMap.Plot
 
-tzoom = [200, 1200]ms
-ax = plotsig(t, vimsig / mV, tzoom; xlabel="Time (s)", hylabel="mV", alpha=0.7);
-plotsig(t, v / mV, tzoom; ax);
-
-# ## Test conntest
-
-example_presynspikes = input_spikes.conn.exc[44]
-plotSTA(vimsig, example_presynspikes, p);
-
-p_value = test_connection(vimsig, example_presynspikes, p)
-
-# ## Conntest performance
-
-N_eval_trains = p.evaluation.num_tested_neurons_per_group
-
-α = 0.05;
-
-function evaluate_conntest_performance(vimsig, input_spikes, p)
-    
-    resetrng!(p.evaluation.rngseed)
-
-    TP_exc = 0
-    TP_inh = 0
-    TP_unconn = 0
-
-    for input_train in input_spikes.conn.exc[1:N_eval_trains]
-        p_value = test_connection(vimsig, input_train, p)
-        if p_value < α
-            TP_exc += 1
-        end
-    end
-
-    for input_train in input_spikes.conn.inh[1:N_eval_trains]
-        p_value = test_connection(vimsig, input_train, p)
-        if p_value > 1 - α
-            TP_inh += 1
-        end
-    end
-
-    for input_train in input_spikes.unconn[1:N_eval_trains]
-        p_value = test_connection(vimsig, input_train, p)
-        if α/2 ≤ p_value ≤ 1 - α/2
-            TP_unconn += 1
-        end
-    end
-
-    TPR_exc    = TP_exc / N_eval
-    TPR_inh    = TP_inh / N_eval
-    TPR_unconn = TP_unconn / N_eval
-    
-    FPR = 1 - TPR_unconn
-    
-    return TPR_exc, TPR_inh, FPR
-end;
-
-evaluate_conntest_performance(vimsig, input_spikes, p)
-
-# ## Performance for given params
-
-function performance_for(p::ExperimentParams)
-    _t, _v, vimsig, input_spikes = sim(p.sim);
-    return evaluate_conntest_performance(vimsig, input_spikes, p)
-end;
-
-VI_params = VoltageImagingParams(;
-    spike_height = cortical_RS.v_peak - cortical_RS.v_rest,
-    spike_SNR = Inf,
-);
-
-durations = [
-    30 * seconds,
-    1 * minutes,
-    5 * minutes,
-    10 * minutes,
-    20 * minutes,
-    30 * minutes,
-]
-xlabels = durations / minutes .|> x -> @sprintf "%.3G" x;
-
 # +
-TPRs_exc = Vector{Float64}()
-TPRs_inh = Vector{Float64}()
-FPRs     = Vector{Float64}()
+xlabels = [p.sim.input.N_conn for p in param_sets]
+xticks = [1:length(xlabels);]
+plot_detection_rate(detection_rate; kw...) = plot(
+    xticks,
+    detection_rate,
+    ".-";
+    ylim=(0, 1),
+    xminorticks=false,
+    clip_on=false,
+    kw...
+)
+ax = plot_detection_rate([p.TPR_exc for p in perfs], label="for excitatory inputs")
+     plot_detection_rate([p.TPR_inh for p in perfs], label="for inhibitory inputs")
+     plot_detection_rate([p.FPR for p in perfs], label="for unconnected spikers")
 
-for duration in durations
-    @show duration / minutes
-    params = ExperimentParams(sim = SimParams(; duration, imaging = VI_params))
-    TPR_exc, TPR_inh, FPR = performance_for(params)
-    @show TPR_exc, TPR_inh, FPR
-    push!(TPRs_exc, TPR_exc)
-    push!(TPRs_inh, TPR_inh)
-    push!(FPRs, FPR)
-    println()
-end
+@unpack α = param_sets[1].evaluation
+ax.axhline(α, color="black", zorder=3, lw=1, linestyle="dashed", label=f"α = {α:.3G}")
+
+# We don't use our `set`, as that undoes our `xminorticks=false` command (bug).
+ax.set_xticks(xticks, xlabels)
+ax.set_xlabel("Number of connected inputs")
+ax.yaxis.set_major_formatter(PyPlot.matplotlib.ticker.PercentFormatter(xmax=1))
+ax.xaxis.grid(false)
+ax.tick_params(bottom=false)
+ax.spines["bottom"].set_visible(false)
+l = ax.legend(title="Detection rate", ncol=2, loc="lower center", bbox_to_anchor=(0.5, 1.1));
+l._legend_box.align = "left";
 # -
 
-xticks = [1:length(durations);]
-plott(rates; kw...) = plot(
-    xticks, rates, ".-"; ylim=(0,1),
-    xminorticks=false, clip_on=false, kw...
-)
-smaller = (lw=1.8, ms=08)
-medium  = (lw=2.0, ms=10)
-larger  = (lw=2.2, ms=12) 
-ax = plott(FPRs;      larger...,  label="Unconnected, falsely detected")
-ax = plott(TPRs_inh;  medium...,  label="Inhibitory, detected ✔")
-ax = plott(TPRs_exc;  smaller..., label="Excitatory, detected ✔")
-ax.set_xticks(xticks, xlabels)
-ax.set_xlabel("Recording duration (minutes)")
-ax.set_ylabel("Fraction of input neurons of type")
-ax.axhline(α, color="black", zorder=3, lw=1, linestyle="dashed", label=f"α = {α:.3G}")
-ax.legend();
-
-
+# - We see monotonic breakdown in excitatory connection detectability.
+# - Same for inhibitory, except it's not monotonic. Fluke due to sampling?
+# - This plot must be improved with multiple simulations per condition rather than just single point.  
+#   (takes a while to run multiple 10' N=6500 sims though).
