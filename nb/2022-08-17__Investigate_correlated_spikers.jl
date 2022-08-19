eb6898f7e2a7f997ccc40caad55a621302905889
Tomas Fiers <tomas.fiers@gmail.com>
2022-08-19 14:59:21 +0100

    sci: other binsizes; and cor baseline (random sample from net)

--- nb/2022-08-17__Investigate_correlated_spikers.jl
+++ nb/2022-08-17__Investigate_correlated_spikers.jl
@@ -118,16 +118,23 @@ using VoltoMapSim.Plot
 
 jn(strs...) = join(strs, "\n");
 
-ax = ydistplot(
-    jn("Unconnected,", "not detected", "↕", "exc inputs") => spikecors(insignif_unconn, ii.exc_inputs),
-    jn("Unconnected,", "not detected", "↕", "inh inputs") => spikecors(insignif_unconn, ii.inh_inputs),
-    jn("Unconnected", "but detected", "↕", "exc inputs") => spikecors(signif_unconn, ii.exc_inputs),
-    jn("Unconnected", "but detected", "↕", "inh inputs") => spikecors(signif_unconn, ii.inh_inputs),
-    figsize = (6, 3),
-    hylabel = "Binned spiketrain correlations with inputs to neuron $m \nBinsize = 100 ms",
-    ylabel = "Pearson correlation",
-)
-add_refline(ax, 0, zorder=1, c="gray");
+function corplot(; binsize)
+    binned_spikes = [bin(s.spike_times[n], duration = 10minutes; binsize) for n in s.neuron_IDs];
+    spikecors(group_A, group_B) = vec([cor(binned_spikes[m], binned_spikes[n]) for m in group_A, n in group_B]);
+    ax = ydistplot(
+        jn("Unconnected,", "not detected", "↕", "exc inputs") => spikecors(insignif_unconn, ii.exc_inputs),
+        jn("Unconnected,", "not detected", "↕", "inh inputs") => spikecors(insignif_unconn, ii.inh_inputs),
+        jn("Unconnected", "but detected (FP)", "↕", "exc inputs") => spikecors(signif_unconn, ii.exc_inputs),
+        jn("Unconnected", "but detected (FP)", "↕", "inh inputs") => spikecors(signif_unconn, ii.inh_inputs),
+        figsize = (6, 3),
+        hylabel = jn("Spike correlations between unconnected & connected neurons, for neuron $m",
+                     "(Binsize = $(binsize/ms) ms)"),
+        ylabel = "Pearson correlation of binned spikes",
+    )
+    add_refline(ax, 0, zorder=1, c="gray")
+    return ax
+end
+ax = corplot(binsize=100ms);
 
 # So the strong-correlation outliers are with inhibitory inputs.
 
@@ -135,16 +142,67 @@ add_refline(ax, 0, zorder=1, c="gray");
 #
 # But we must zoom in a bit:
 
-set(ax, ylim=[-0.08, 0.08], xtype=:keep)
-ax.get_children() .|> (ch -> ch.set_clip_on(true))
+set(ax, ylim=(-0.08, 0.08), xtype=:keep)
 ax.figure
 
 # We expected the FP correlations with exc to be higher.
 # But it's higher with inh.
 # That makes sense though as the inh→exc connections were better detected than the exc→exc. (neuron `1` is exc).
 
+# + [markdown] heading_collapsed=true
+# ### Other binsizes
+
+# + hidden=true
+corplot(binsize=200ms);
+
+# + hidden=true
+corplot(binsize=50ms);
+
+# + hidden=true
+corplot(binsize=25ms);
+
+# + hidden=true
+corplot(binsize=10ms);
+
+# + hidden=true
+corplot(binsize=5ms);
+
+# + [markdown] hidden=true
+# So the outliers remain for binsizes 50 and 200, and the FP ↔ inh nonzero corr seems to too.
+# For binsize 25 ms however, both phenomena disappear; but then for 10 and 5 ms, the nonzero corr seems to be back.
+# -
+
+# ## All spike correlations in network
+
+# We need to sample, as calculating 1000x1000 spiketrain correlations takes too long.
+
+ns = sample(s.neuron_IDs, 100, replace=false);
+
+spikecors_nosame(A,B) = vec([cor(binned_spikes[m], binned_spikes[n]) for m in A, n in B if m != n]);
+
+ydistplot(""=>spikecors_nosame(ns,ns), ref=0, hylabel=jn("Spike correlations of random neuron sample", "(Binsize 100 ms)"));
+
+# So the 0.15 to 0.35 outliers we saw are in fact rare.
+
+# Split by neuron types
+
+ns_exc = [m for m in ns if m in s.neuron_IDs.exc]
+ns_inh = [m for m in ns if m in s.neuron_IDs.inh];
+
+ydistplot(
+    "exc ↔ exc" => spikecors_nosame(ns_exc, ns_exc),
+    "exc ↔ inh" => spikecors_nosame(ns_exc, ns_inh),
+    "inh ↔ inh" => spikecors_nosame(ns_inh, ns_inh),
+    ref=0,
+    hylabel=jn("Spike correlations of random neuron sample", "(Binsize 100 ms)")
+);
+
+# inhibitory neurons seem on average to be more correlated with other neurons than excitatory neurons are.
+
 # ## Investigate outliers
 
 # What do their STA's and spiketrains look like?  
 # How are they connected in network?  
 # Why does that one have high cor but is still undetected?
+
+
