17abe2e5cba597bcff1ee74fd1a9e2098d60ac80
Tomas Fiers <tomas.fiers@gmail.com>
2022-08-23 11:08:19 +0200

    finish eval func with `area`: one loop, detrates. Apply for 1 and 801.

--- nb/2022-08-21__Area-under-STA.jl
+++ nb/2022-08-21__Area-under-STA.jl
@@ -131,7 +131,7 @@ perfn.detection_rates
 
 # Again, signifcantly worse performance.
 
-# ## Use `area` just for deciding exc or inh
+# ## Use `area` for deciding exc or inh
 
 # This was the original motivation for this measure.
 
@@ -209,38 +209,121 @@ test_connection_and_type(ii.v, ii.spiketrains.conn.inh[1], p)
 
 using DataFrames
 
+# +
+# @cached key=[:m,:p] (
 function evaluate_conntest_perf_v2(s, m, p)
     # s = augmented simdata
     # m = postsynaptic neuron ID
-    ii = s.input_info[m]
     @unpack N_tested_presyn, rngseed = p.evaluation;
     resetrng!(rngseed)
-    function get_sample(IDs)
+    function get_IDs_labels(IDs, label)
         N = min(length(IDs), N_tested_presyn)
-        return sample(IDs, N, replace = false, ordered = true)
+        IDs_sample = sample(IDs, N, replace = false, ordered = true)
+        return zip(IDs_sample, fill(label, N))
     end
-    df = DataFrame(
+    ii = s.input_info[m]
+    IDs_labels = chain(
+        get_IDs_labels(ii.exc_inputs, :exc),
+        get_IDs_labels(ii.inh_inputs, :inh),
+        get_IDs_labels(ii.unconnected_neurons, :unconn),
+    )
+    tested_neurons = DataFrame(
         input_neuron_ID = Int[],     # global ID
         real_type       = Symbol[],  # :unconn, :exc, :inh
         predicted_type  = Symbol[],  # idem
         pval            = Float64[],
         area_over_start = Float64[],
     )
-    function test(IDs, real_type)
-        for n in get_sample(IDs)
-            o = test_connection_and_type(ii.v, s.spike_times[n], p)
-            push!(df, Dict(pairs((; input_neuron_ID = n, real_type, o...))))
-        end
-    end
-    test(ii.exc_inputs, :exc)
-    test(ii.inh_inputs, :inh)
-    test(ii.unconnected_neurons, :unconn)
-    #return perf = (; df, detection_rates = (; TPR_exc, TPR_inh, FPR))
-    return df
-end;
+    @showprogress (every = 400ms) "Testing connections: " (
+    for (n, label) in collect(IDs_labels)  # `collect` necessary somehow
+        test_result = test_connection_and_type(ii.v, s.spike_times[n], p)
+        row = (input_neuron_ID = n, real_type = label, test_result...)
+        push!(tested_neurons, Dict(pairs(row)))
+    end)
+    tn = tested_neurons
+    det_rate(t) = count((tn.real_type .== t) .& (tn.predicted_type .== t)) / count(tn.real_type .== t)    
+    detection_rates = (
+        TPR_exc = det_rate(:exc),
+        TPR_inh = det_rate(:inh),
+        FPR = 1 - det_rate(:unconn),
+    )
+    return (; tested_neurons, detection_rates)
+end
+# end)
 
-df = evaluate_conntest_perf_v2(s, 1, p);
+cached_eval(s, m, p) = cached(evaluate_conntest_perf_v2, [s, m, p], key = [m, p]);
+# -
+
+# Aside: @cached macro wish.  
+# Also generates a $(funcname)_uncached.
+# So here: evaluate_conntest_perf_uncached
+
+# +
+# @profview evaluate_conntest_perf_v2(s, 1, p);
+# -
+
+# ^ Most time spent in the `+` and the `setindex` of the `calc_STA` loop.
+# (A bit also in `shuffle_ISIs`).
+# (Could speed up a bit maybe by having STA on the stack -- prob using StaticArrays.jl -- to save that `setindex` time.
+# Though wouldn't be major, as most time is still spent in float `+`). Another thing to try is `@fastmath` IEEE rules relaxing, to speed up the `+`.
+
+testeval = cached_eval(s, 1, p);
 
 ENV["LINES"] = 100
+
+testeval.detection_rates
+
+# Comparison with before, when we didn't predict the type (just connected or not) (from `2022-07-05__Network-conntest`):
+#
+# (TPR_exc = 0.154, TPR_inh = 1, FPR = 0.15)
+
+# The exc performance drops to zero.
+# This is because all previously detected exc inputs are still detected, but classified as inhibitory, as seen in the below table.
+#
+# The FPR rate is different because we now take a random sample of unconnected neurons to test (before, we took the first 40).
+
+testeval.tested_neurons
+
+# ### Inhibitory postsynaptic neuron
+
+# Now test input connections to the recorded inhibitory neuron:
+
+te_inh = cached_eval(s, 801, p);
+
+te_inh.detection_rates
+
+# Comparison with before, as above:
+#
+# `(TPR_exc = 0.714, TPR_inh = 0.9, FPR = 0.05)`
+
+# Results per neuron below.
+# The `area_over_start` seems to work: exc is mostly positive (and thus predicted as exc), and vice versa for inhibitory/negative.
+
+te_inh.tested_neurons
+
+# We missclassify an inhibitory input as excitatory because it's "area over start" is positive.
+# What does the STA look like?
+
+plotSTA(s.signals[801].v, s.spike_times[846], p);
+
+# Looks nicely downwards..
+
+area(calc_STA(s.signals[801].v, s.spike_times[846], p))
+
+# But the area-over-start is indeed positive.
+#
+# Could be remedied with a shorter STA window length.
+
+# For comparison, the STA of a correctly identified inh input:
+
+plotSTA(s.signals[801].v, s.spike_times[815], p);
+
+# Looks very similar (the relative height of the positive and negative bumps).
+#
+# The problem (the difference) is at t0.
+
+# ---
+# Unrelated question (on previous results):
+# Why are exc input STA's on inh good, but on exc neurons so bad?
 
 
