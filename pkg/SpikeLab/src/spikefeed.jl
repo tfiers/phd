0dfb5ab0bcdc8656348075e248ce14a89ad11d68
Tomas Fiers <tomas.fiers@gmail.com>
2022-10-28 18:57:07 +0100

    SpikeLab: doctest, docs/make.jl

--- pkg/SpikeLab/src/spikefeed.jl
+++ pkg/SpikeLab/src/spikefeed.jl
@@ -3,32 +3,10 @@
 
 Keeps track of how many spikes have been processed already.
 
-Used with [`advance_to!`](@ref) in a simulation loop, to check whether an input spike train
-has spiked in the current timestep (and if so, how many times).
+Used with [`advance_to!`](@ref) in the simulation loop, to check whether an input spike
+train has spiked in the current timestep (and if so, how many times).
 
-# Usage
-
-    julia> x = [1.0, 2.0, 4.0];  # The provided spike times must already be sorted
-
-    julia> sf = SpikeFeed(x)
-    SpikeFeed [0/3] (next: 1.0)  # [0/3] is the [seen/total] number of spikes
-
-    julia> advance_to!(sf, 3.0)  # Provide the current time
-    2                            # Get the number of new spikes
-
-    julia> advance_to!(sf, 3.0)
-    0
-
-    julia> sf
-    SpikeFeed [2/3] (next: 4.0)
-
-    julia> advance_to!(sf, 4.0)  # We advance up to *and including* the given time
-    1
-
-    julia> sf
-    SpikeFeed [3/3] (exhausted)
-
-~
+See [Usage](@ref).
 """
 struct SpikeFeed
     spikes  ::Vector{Float64}  # Spike times, assumed sorted
@@ -37,7 +15,7 @@ end
 SpikeFeed(spikes) = SpikeFeed(spikes, Ref(1))
 
 time_of_next(sf::SpikeFeed) = sf.spikes[sf.next[]]
-is_exhausted(sf::SpikeFeed) = sf.next[] > length(sf.spikes); sort!()
+is_exhausted(sf::SpikeFeed) = sf.next[] > length(sf.spikes)
 
 """
     n = advance_to!(sf::SpikeFeed, t)
@@ -46,10 +24,9 @@ Count the number of spikes `n` in the time interval `(tₚ, t]`,
 where `tₚ` is the time `t` this method was last called with.
 (On the first call, count all spikes before `t`).
 
-In a simulation loop, `t - tₚ` will be the timestep, `Δt`,
-and this method will count the number of spikes in the current timestep.
+When used in a simulation loop, `t - tₚ` will typically be the timestep, `Δt`.
 
-See [`SpikeFeed`](@ref) for an example.
+See [Usage](@ref).
 """
 advance_to!(sf::SpikeFeed, t) = begin
     n = 0
