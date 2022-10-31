d4ac14578c2bf6091022d10deab0c367fcdd6303
Tomas Fiers <tomas.fiers@gmail.com>
2022-10-31 13:49:30 +0000

    `const` units (for type inference)

--- pkg/VoltoMapSim/src/units.jl
+++ pkg/VoltoMapSim/src/units.jl
@@ -2,79 +2,79 @@
 # 'Fake' units, until a better solution exists.
 # (`Unitful` works great, but the interactive UX is bad because of its very verbose types).
 
-giga  = 1e9
-mega  = 1e6
-kilo  = 1e3
-milli = 1e-3
-centi = 1e-2
-micro = 1e-6
-nano  = 1e-9
-pico  = 1e-12
+const giga  = 1e9
+const mega  = 1e6
+const kilo  = 1e3
+const milli = 1e-3
+const centi = 1e-2
+const micro = 1e-6
+const nano  = 1e-9
+const pico  = 1e-12
 export mega, kilo, milli, centi, micro, nano, pico
 
-second = 1.0
-Hz = 1 / second
-ms = milli * second
-minute = 60 * second
-hour = 60 * minute
+const second = 1.0
+const Hz = 1 / second
+const ms = milli * second
+const minute = 60 * second
+const hour = 60 * minute
 export second, Hz, ms, minute, hour
 
-seconds = second
-minutes = minute
-hours = hour
+const seconds = second
+const minutes = minute
+const hours = hour
 export seconds, minutes, hours
 # Plural makes more sense in e.g. `10minutes`.
 # Singular makes more sense in `f(x, unit = second)` or `x = 80 * second`.
 
-metre = meter = 1.0
-cm = centi * metre
-mm = milli * metre
-μm = um = micro * metre
-nm = nano * metre
+const metre = meter = 1.0
+const cm = centi * metre
+const mm = milli * metre
+const μm = um = micro * metre
+const nm = nano * metre
 export metre, meter, cm, mm, μm, um, nm
 
-ampere = 1.0
-mA = milli * ampere
-uA = μA = micro * ampere
-nA = nano * ampere
-pA = pico * ampere
-export ampere, mA, μA, uA, nA, pA
+const ampere = 1.0
+const mA = milli * ampere
+const μA = micro * ampere
+const nA = nano * ampere
+const pA = pico * ampere
+export ampere, mA, μA, nA, pA
 
-volt = 1.0
-mV = milli * volt
-uV = μV = micro * volt
-nV = nano * volt
-export volt, mV, uV, μV, nV
+const volt = 1.0
+const mV = milli * volt
+const μV = micro * volt
+const nV = nano * volt
+export volt, mV, μV, nV
 
-siemens = ampere / volt
-ohm = 1 / siemens
-mS = milli * siemens
-nS = nano * siemens
-pS = pico * siemens
-Mohm = mega * ohm
-Gohm = giga * ohm
+const siemens = ampere / volt
+const ohm = 1 / siemens
+const mS = milli * siemens
+const nS = nano * siemens
+const pS = pico * siemens
+const Mohm = mega * ohm
+const Gohm = giga * ohm
 export siemens, ohm, mS, nS, pS, Mohm
 
-coulomb = ampere * second
-farad = coulomb / volt
-uF = μF = micro * farad
-nF = nano * farad
-pF = pico * farad
-export coulomb, farad, uF, μF, nF, pF
+const coulomb = ampere * second
+const farad = coulomb / volt
+const μF = micro * farad
+const nF = nano * farad
+const pF = pico * farad
+export coulomb, farad, μF, nF, pF
 
-kibi = 2^10
-mebi = 2^20
-gibi = 2^30
+const kibi = 2^10
+const mebi = 2^20
+const gibi = 2^30
 # Sidenote: Windows uses binary prefixes, but displays them as SI: "MB" is actually "MiB".
 
-bytes = 1
+const bytes = 1
     # We choose bytes as base unit (instead of bits), as that is what e.g. `sizeof` and
     # `Base.summarysize` report.
-bits = bytes / 8
-kB = kilo * bytes
-MB = mega * bytes
-GB = giga * bytes
-kiB = kibi * bytes
-MiB = mebi * bytes
-GiB = gibi * bytes
+const bits = bytes / 8
+const kB = kilo * bytes
+const MB = mega * bytes
+const GB = giga * bytes
+const kiB = kibi * bytes
+const MiB = mebi * bytes
+const GiB = gibi * bytes
 export bytes, bits, kB, kiB, MB, MiB, GB, GiB
