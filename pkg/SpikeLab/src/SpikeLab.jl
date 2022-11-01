49c0797f57fb81715374ebc64468f2874c79963c
Tomas Fiers <tomas.fiers@gmail.com>
2022-11-01 22:42:13 +0000

    shorter main module file

--- pkg/SpikeLab/src/SpikeLab.jl
+++ pkg/SpikeLab/src/SpikeLab.jl
@@ -1,31 +1,21 @@
 module SpikeLab
 
+using Reexport
 using Distributions
-# Don't `@reexport`: this macro somehow also exports our own `LogNormal` (see below),
-# creating a conflict.
-using ComponentArrays: CVector  # alias for ComponentVector
+# ↪ Don't `@reexport Distriubtions`: this macro somehow also exports our own `LogNormal`
+#   (see below), creating a conflict.
+@reexport using ComponentArrays: CVector                # Alias for ComponentVector
+@reexport using ComponentArrays: Axis, ComponentVector  # For unqualified typenames in errors
 
 include("units.jl")
-
 include("spikefeed.jl")
-
 include("distributions.jl")
-# Don't export LogNormal, to not conflict with Distributions.jl
-# Instead, use `SpikeLab.LogNormal` to use our parametrization.
+# ↪ Don't export LogNormal, to not conflict with Distributions.jl
+#   Instead, use `SpikeLab.LogNormal` to use our parametrization.
+include("misc.jl");     export to_timesteps
+include("eqparse.jl");  export @eqs
+include("sim.jl");      export Model, sim, init_sim, step!, SimState
+include("poisson.jl");  export poisson_spikes, poisson_input
+include("latex.jl");    export show_eqs
 
-include("misc.jl")
-export to_timesteps
-
-include("eqparse.jl")
-export @eqs
-
-include("sim.jl")
-export Model, sim
-
-include("poisson.jl")
-export poisson_spikes, poisson_input
-
-include("latex.jl")
-export show_eqs
-
-end
+end # module
