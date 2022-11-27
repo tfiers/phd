6eb473700336a4f652089560965f91465abef03f
Tomas Fiers <tomas.fiers@gmail.com>
2022-11-27 14:39:19 +0000

    Get rid of custom merge_sort
    way slower and more allocs than the `quicksort!(reduce(vcat, vecs))` way.
    
    from profview, lotsa time was spent on
    `for j in active: curval[j] < smalest?`
    So an optimization could be to use a priorityqueue here. When updating the pointer of the vec with the smallest el, put the new val in a pq.

--- pkg/SpikeLab/src/spiketrain.jl
+++ pkg/SpikeLab/src/spiketrain.jl
@@ -33,53 +33,7 @@ Base.merge(trains::AbstractVector{SpikeTrain}) =
         checksorted = false,
     )
 
-
-"""
-    merge_sorted(vecs)
-
-Given a list of sorted vectors, return a single sorted vector of all elements.
-
-Not to be confused with mergesort. (Although this function _does_ do the same thing as the
-`merge` part in mergesort -- only for an arbitrary number of arrays, instead of two).
-"""
-function merge_sorted(vecs)
-    T = typejoin(eltype.(vecs)...)
-    N = sum(length, vecs)
-    merged = Vector{T}(undef, N)
-    vecs = [v for v in vecs if length(v) > 0]
-    curval = @inbounds [v[1] for v in vecs]
-    curptr = [1 for v in vecs]
-    active = BitSet(1:length(vecs))  # Vecs that are not exhausted yet
-    @inbounds for i in 1:N
-        # Find the smallest of the active vecs' current values
-        # (We cannot use `findmin(curval[j] for j in active)`, as there's no `keys(::BitSet)`).
-        smallest = nothing
-        ĵ = nothing
-        for j in active
-            val = curval[j]
-            if isnothing(smallest) || val < smallest
-                smallest = val
-                ĵ = j
-            end
-        end
-        # ..and add it to the output list
-        merged[i] = smallest
-        # Advance the current-element pointer of vec ĵ
-        k = (curptr[ĵ] += 1)
-        vec = vecs[ĵ]
-        if k > length(vec)      # Check if exhausted
-            delete!(active, ĵ)  # If so, remove from active set
-        else
-            curval[ĵ] = vec[k]  # Otherwise, update the current value
-        end
-    end
-    return merged
-end
-#
-# There is https://github.com/vvjn/MergeSorted.jl
-# but that is only for two vectors, not an arbitrary number.
-# (What is perf of applying this recursively?)
-#
-# Also, `sort!(reduce(vcat, vecs))` might be efficient enough:
-# "QuickSort usually performs well on pre-sorted arrays"
-# (https://stackoverflow.com/a/35841650/2611913)
+merge_sorted(vecs) = sort!(reduce(vcat, vecs))
+# This implementation does not explicitly make use of the fact that the vecs are already
+# sorted. But quicksort performs well here. Much better than a specific implementation of
+# `merge_sorted` I wrote (see this file and `test/merge_sorted.jl` in commit 15ec0d9).
