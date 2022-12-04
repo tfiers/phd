1ae7463ed66fd619f3453bd8fd04ad1eb1f79d9f
Tomas Fiers <tomas.fiers@gmail.com>
2022-12-04 03:21:22 +0000

    confusionmatrix.jl: all in @kwdef struct, no more 'metrics' namedtuple. also, use pretty tables for table fmt

--- pkg/VoltoMapSim/src/infer/confusionmatrix.jl
+++ pkg/VoltoMapSim/src/infer/confusionmatrix.jl
@@ -1,47 +1,56 @@
-using MyToolbox: @NT
+using Base: @kwdef
+using Printf
+using PrettyTables
 
-struct ConfusionMatrix
-    TP::Int  # (real +, predicted +)   correct detections
-    TN::Int  # (real -, predicted -)   correct rejections
-    FP::Int  # (real -, predicted +)   wrong detections (missed rejections). Type I error
-    FN::Int  # (real +, predicted -)   missed detections (wrong rejections). Type II error
-end
-
-Base.propertynames(cm::ConfusionMatrix) = keys(metrics(cm))
-Base.getproperty(cm::T, name::Symbol) where T <: ConfusionMatrix =
-    (name in fieldnames(T)) ? getfield(cm, name) : metrics(cm)[name]
-
-metrics(cm::ConfusionMatrix) = @NT begin
-    @unpack TP, TN, FP, FN = cm
-    P  = TP + FN   # Positive
-    N  = TN + FP
-    PP = TP + FP   # Predicted positive
-    PN = TN + FN
-    TPR = TP / P   # True positive rate / recall / sensitivity / power
-    PPV = TP / PP  # Positive predictive value / precision
-    NPV = TN / PN  # Negative predictive value
-    FPR = FP / N   # False positive rate / α
-    FDR = FP / PP  # 1 - PPV. False discovery rate
-    FNR = FN / P   # 1 - TPR. β
-    TNR = TN / N   # 1 - FPR.
-    FOR = FN / PN  # 1 - NPV. False omission rate ('omitted from detections')
-    correct_detections = harmonic_mean(TPR, PPV)  # TP proportions. Known as F1.
-    wrong_detections   = harmonic_mean(FPR, FDR)  # FP proportions. Aka missed rejections.
-    correct_rejections = harmonic_mean(TNR, NPV)  # TN proportions
-    missed_detections  = harmonic_mean(FNR, FOR)  # FN proportions. Aka wrong rejections.
+@kwdef struct ConfusionMatrix
+    TP  ::Int  # (real +, predicted +)   correct detections
+    FP  ::Int  # (real -, predicted +)   wrong detections (missed rejections). Type I error
+    FN  ::Int  # (real +, predicted -)   missed detections (wrong rejections). Type II error
+    TN  ::Int  # (real -, predicted -)   correct rejections
+    # ↑ input
+    # ↓ calculated
+    P    = TP + FN   # Positive
+    N    = TN + FP
+    PP   = TP + FP   # Predicted positive
+    PN   = TN + FN
+    TPR  = TP / P   # True positive rate / recall / sensitivity / power
+    PPV  = TP / PP  # Positive predictive value / precision
+    NPV  = TN / PN  # Negative predictive value
+    FPR  = FP / N   # False positive rate / α
+    FDR  = FP / PP  # = 1 - PPV. False discovery rate
+    FNR  = FN / P   # = 1 - TPR. False negative rate / β
+    TNR  = TN / N   # = 1 - FPR.
+    FOR  = FN / PN  # = 1 - NPV. False omission rate ('omitted from detections')
+    # - 'New' measures (only first is well known, as F1):
+    TP_rel  = harmonic_mean(TPR, PPV)  # Correct detections. Known as F1
+    FP_rel  = harmonic_mean(FPR, FDR)  # Wrong detections / missed rejections
+    FN_rel  = harmonic_mean(FNR, FOR)  # Missed detections / wrong rejections
+    TN_rel  = harmonic_mean(TNR, NPV)  # Correct rejections
+    # - Measures summarizing all four cases:
     accuracy = (TP + TN) / (P + N)
-    MCC = (TP*TN - FP*FN) / sqrt(PP * PN * P * N)  # Matthew's Correlation or Phi Coefficient.
+    MCC      = (TP*TN - FP*FN) / sqrt(PP * PN * P * N)  # Matthew's correlation coefficient  / phi coefficient.
 end
 
 harmonic_mean(x...) = 1 / mean(1 ./ x)
 
-function Base.show(io::IO, ::MIME"text/plain", cm::ConfusionMatrix)
-    println(io, "ConfusionMatrix:")
-    names = propertynames(cm)
-    len(n) = length(string(n))
-    tabs = [0, 4, maximum(len, names)]
-    pad(name) = lpad(name, tabs[findlast(len(name) .> tabs) + 1])
-    for name in names
-        println(io, pad(name), ": ", getproperty(cm, name))
-    end
+Base.show(io::IO, m::MIME"text/plain", cm::ConfusionMatrix) = showtable(io, m, cm)
+Base.show(io::IO, m::MIME"text/html", cm::ConfusionMatrix) = showtable(io, m, cm)
+
+showtable(io, m, o) = pretty_table(io, propertytable(o); tablestyle(m,o)...)
+
+function propertytable(o)
+    names = collect(propertynames(o))
+    vals = [getproperty(o, n) for n in names]
+    return hcat(names, vals)
 end
+tablestyle(m, ::T) where T = (
+    title = "$(T):",
+    show_header = false,
+    formatters = (v,i,j)->fmt(v),
+    body_hlines = cumsum([4, 4, 8]),  # This is ConfusionMatrix specific (so no, not for 'T')
+    backend = table_backend(m),
+)
+fmt(x::Float64) = @sprintf "%.0f%%" 100x   # Or: fmt_pct(x, 1)  # (From ../misc)
+fmt(x) = x
+table_backend(::MIME"text/plain") = Val(:text)
+table_backend(::MIME"text/html") = Val(:html)
