0e7f8e4b8859e2ff5e485e244d7ab9f8dffc0222
Tomas Fiers <tomas.fiers@gmail.com>
2023-08-06 08:50:18 +0200

    dump

--- nb/2023-08-05__AdEx_Nto1_we_sweep.py
+++ nb/2023-08-05__AdEx_Nto1_we_sweep.py
@@ -17,13 +17,6 @@
 
 # %run lib/neuron.py
 
-# We're gonna try a brian trick to try and speed up the simulation:
-# instead of 6500 separate Poisson spike trains, simulate most of them (except a few that we want to use for conntesting) as **one** process. (One for exc and one for inh, to be precise).
-
-μₓ = 4 * Hz
-σ = sqrt(0.6)
-μ = log(μₓ / Hz) - σ**2 / 2
-
 # +
 we = 14 * pS
 wi = 4 * we
